7cc0fd1730f6059357a90090eee70450a22b3203
NeoRay <77061844+NeoRay2100@users.noreply.github.com>
2024-02-02 16:28:59 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         你是第  <span id="busuanzi_value_site_pv"></span>  个试图说服丈母娘的人
       </span>
       <p className="mt-4 text-sm text-slate-500 sm:mt-2">
-        如果你战胜了丈母娘，相信我，困难刚刚开始，去哄哄模拟器试试哄哄女朋友吧
+        如果你战胜了丈母娘，相信我，困难刚刚开始，去<a href="https://hong.azhubaby.com/">哄哄模拟器</a>试试哄哄女朋友吧
       </p>
     </div>
   );
