b8401fb9368ede71ad69bb92eb27495e613842e1
NeoRay <77061844+NeoRay2100@users.noreply.github.com>
2024-02-02 16:15:51 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className="text-center text-xs text-gray-400">
       <span id="busuanzi_container_site_pv">
-        本站总访问量<span id="busuanzi_value_site_pv"></span>人次
+        你是第<span id="busuanzi_value_site_pv"></span>试图说服丈母娘的人
       </span>
       <p className="mt-4 text-sm text-slate-500 sm:mt-2">
-        Copyright © hong.azhubaby.com rights reserved.
+        如果你战胜了丈母娘，相信我，困难刚刚开始，去哄哄模拟器试试哄哄女朋友吧
       </p>
     </div>
   );
