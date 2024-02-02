4e1a603cd2108d017445c1d0d18ae732cd2a4921
NeoRay <77061844+NeoRay2100@users.noreply.github.com>
2024-02-02 16:14:10 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,15 @@
 import Link from "next/link";
-import {
-    VercelIcon,
-    GithubIcon,
-} from "@/components/Icons";
+import { VercelIcon, GithubIcon } from "@/components/Icons";
 
 export default function Footer() {
-    return (
-        <div className="text-center text-xs text-gray-400">
-            <Link
-                target="_blank"
-                href="https://azhubaby.com"
-                aria-label="developer"
-                className="text-slate-600">
-                开发者
-            </Link>
-            {" "}| {" "}
-            <Link
-                target="_blank"
-                href="https://hong.azhubaby.com/privacy"
-                aria-label="Privacy"
-                className="text-slate-600">
-                Privacy
-            </Link>
-            {" "}| {" "}
-            <Link
-                target="_blank"
-                href="https://hong.azhubaby.com/terms"
-                aria-label="Terms of Service"
-                className="text-slate-600">
-                terms
-            </Link>
-            <p className="mt-4 text-sm text-slate-500 sm:mt-2">
-                Copyright © hong.azhubaby.com rights reserved.
-            </p>
-        </div>
-    );
+  return (
+    <div className="text-center text-xs text-gray-400">
+      <span id="busuanzi_container_site_pv">
+        本站总访问量<span id="busuanzi_value_site_pv"></span>人次
+      </span>
+      <p className="mt-4 text-sm text-slate-500 sm:mt-2">
+        Copyright © hong.azhubaby.com rights reserved.
+      </p>
+    </div>
+  );
 }
