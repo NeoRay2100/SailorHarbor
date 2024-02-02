8097f159a82aafeeb6bb27aada0402c4e0ead0c0
NeoRay <77061844+NeoRay2100@users.noreply.github.com>
2024-02-02 16:25:17 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="text-center text-xs text-gray-400">
       <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
       <span id="busuanzi_container_site_pv">
-        你是第  <span id="busuanzi_value_site_pv"></span>  试图说服丈母娘的人
+        你是第  <span id="busuanzi_value_site_pv"></span>  个试图说服丈母娘的人
       </span>
       <p className="mt-4 text-sm text-slate-500 sm:mt-2">
         如果你战胜了丈母娘，相信我，困难刚刚开始，去哄哄模拟器试试哄哄女朋友吧
